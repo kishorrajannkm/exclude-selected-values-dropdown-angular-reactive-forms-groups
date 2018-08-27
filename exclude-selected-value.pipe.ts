18b8124624431373137559c2587f5f993975659b
kishorrajannkm <42717246+kishorrajannkm@users.noreply.github.com>
2018-08-27 10:54:55 +0530

    Update exclude-selected-value.pipe.ts

--- exclude-selected-value.pipe.ts
+++ exclude-selected-value.pipe.ts
@@ -6,12 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ExcludeSelectedValuePipe implements PipeTransform {
 
   transform(fullArray: any, allSelected:any, currentSelection: any): any {
-    console.log(allSelected);
     let newOptionsList = [];
     for(let i = 0; i < fullArray.length; i++){
       let addToArray=true;
       for(let j=0;j<allSelected.length;j++){
-        //console.log(allSelected[j].mainKey+"   "+fullArray[i].mainKey);
         if(allSelected[j].mainKeyChosen===fullArray[i].mainKey){
             addToArray=false;
         }
